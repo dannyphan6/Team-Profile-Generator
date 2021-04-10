14524bfd45d22264912a0620c1f3e66573490661
Daniel Phan <dannyphan@comcast.net>
2021-04-10 03:40:30 -0700

    Refactored classes for each type of employee

--- index.js
+++ index.js
@@ -6,13 +6,93 @@ const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
 // Final list of all team members 
-let finalTeam = [];
+let finalTeamEngineer = [];
+let finalTeamManager = [];
+let finalTeamIntern = [];
 
 function createPage() {
-    fs.writeFile(index.html, response, err => {
+    fs.writeFile("index.html", `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <title>Team Profile Generator</title>
+</head>
+<body>
+    <main>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+              <h1 class="display-4">Team Profile Generator</h1>
+              </div>
+              ${createCardManager()}
+              ${createCardEngineer()}
+              ${createCardIntern()}
+          </div>
+    </main>
+</body>
+</html>`, err => {
         if (err) throw err;
     });
 };
+
+function createCardEngineer() {
+    let team = ""
+    for (let i = 0; i < finalTeamEngineer.length; i++) {
+        team += `
+        <div class="card bg-primary text-white">
+            <div class="card-body">
+                <h3 class="card-title">${finalTeamEngineer[i].name}</h3>
+                <h4 class="card-subtitle"><i class="fas fa-mug-hot">${finalTeamEngineer[i].role}</i></h4>
+                <p>ID: ${finalTeamEngineer[i].id}</p>
+                <p>Email: ${finalTeamEngineer[i].email}</p>
+                <p>GitHub: ${finalTeamEngineer[i].github}</p>
+            </div>
+        </div>
+        `
+    };
+    return team;
+};
+
+function createCardManager() {
+    let team = ""
+    for (let i = 0; i < finalTeamManager.length; i++) {
+        team += `
+        <div class="card bg-primary text-white">
+            <div class="card-body">
+                <h3 class="card-title">${finalTeamManager[i].name}</h3>
+                <h4 class="card-subtitle"><i class="fas fa-mug-hot">${finalTeamManager[i].role}</i></h4>
+                <p>ID: ${finalTeamManager[i].id}</p>
+                <p>Email: ${finalTeamManager[i].email}</p>
+                <p>Office Number: ${finalTeamManager[i].officeNumber}</p>
+            </div>
+        </div>
+        `
+    };
+    return team;
+};
+
+function createCardIntern() {
+    let team = ""
+    for (let i = 0; i < finalTeamIntern.length; i++) {
+        team += `
+        <div class="card bg-primary text-white">
+            <div class="card-body">
+                <h3 class="card-title">${finalTeamIntern[i].name}</h3>
+                <h4 class="card-subtitle"><i class="fas fa-mug-hot">${finalTeamIntern[i].role}</i></h4>
+                <p>ID: ${finalTeamIntern[i].id}</p>
+                <p>Email: ${finalTeamIntern[i].email}</p>
+                <p>School: ${finalTeamIntern[i].school}</p>
+            </div>
+        </div>
+        `
+    };
+    return team;
+};
+
 
 function addMember() {
     const question = [
@@ -24,20 +104,20 @@ function addMember() {
         },
     ];
 
-    return inquirer.prompt(question).then( response => {
-        switch(response.addNew) {
+    return inquirer.prompt(question).then(response => {
+        switch (response.addNew) {
             case "Manager":
                 managerQuestions();
-            break;
+                break;
             case "Engineer":
                 engineerQuestions();
-            break;
+                break;
             case "Employee":
                 employeeQuestions();
-            break;
+                break;
             case "Intern":
                 internQuestions();
-            break;
+                break;
             default:
                 createPage();
         };
@@ -71,17 +151,17 @@ function managerQuestions() {
     // Creates new "Manager" with user response to questions
     return inquirer.prompt(questions).then(response => {
         const manager = new Manager(
-            response.managerName, 
-            response.managerId, 
-            response.managerEmail, 
+            response.managerName,
+            response.managerId,
+            response.managerEmail,
             response.managerOffice
         );
 
+        finalTeamManager.push(manager);
+        // console.log(finalTeam);
+
         addMember();
 
-        finalTeam.push(manager);
-        // console.log(finalTeam);
-    
     });
 };
 
@@ -112,7 +192,7 @@ function engineerQuestions() {
     ];
 
     // Creates new "Engineer" with user response to questions
-    return inquirer.prompt(questions).then( response => {
+    return inquirer.prompt(questions).then(response => {
         const engineer = new Engineer(
             response.engineerName,
             response.engineerId,
@@ -120,10 +200,10 @@ function engineerQuestions() {
             response.engineerGitHub
         );
 
-        addMember();
-
-        finalTeam.push(engineer);
+        finalTeamEngineer.push(engineer);
         // console.log(finalTeam);
+
+        addMember();
 
     });
 };
@@ -148,17 +228,17 @@ function employeeQuestions() {
     ];
 
     // Creates new "Employee" with user response to questions
-    return inquirer.prompt(questions).then( response => {
+    return inquirer.prompt(questions).then(response => {
         const employee = new Employee(
             response.employeeName,
             response.employeeId,
             response.employeeEmail
         );
 
-        addMember();
-
-        finalTeam.push(employee);
+        finalTeamEmployee.push(employee);
         // console.log(finalTeam);
+
+        addMember();
 
     });
 };
@@ -188,7 +268,7 @@ function internQuestions() {
     ];
 
     // Creates new "Intern" with user response to questions
-    return inquirer.prompt(questions).then( response => {
+    return inquirer.prompt(questions).then(response => {
         const intern = new Intern(
             response.internName,
             response.internId,
@@ -196,10 +276,10 @@ function internQuestions() {
             response.internSchool
         );
 
-        addMember();
-
-        finalTeam.push(intern);
+        finalTeamIntern.push(intern);
         // console.log(finalTeam);
+
+        addMember();
 
     });
 };
